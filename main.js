7ea3e52bb97a81df3565c339904f463f2606ea26
Simone Fortunato <frtsmn94@gmail.com>
2022-07-21 16:48:32 +0200

    parte cambio colore a like

--- main.js
+++ main.js
@@ -72,48 +72,52 @@ for ( let i=0; i<posts.length; i++){
     
 
     container.innerHTML += 
-    `<div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${posts[i].media}" alt=${nomeAutore}>                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${nomeAutore}</div>
-                <div class="post-meta__time">${posts[i].created}</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">${posts[i].content}</div>
-    <div class="post__image">
-        <img src="${immagineAutore }" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" id="bottoneLike" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label" id="testoTasto">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>`
+                    `<div class="post">
+                    <div class="post__header">
+                        <div class="post-meta">                    
+                            <div class="post-meta__icon">
+                                <img class="profile-pic" src="${posts[i].media}" alt=${nomeAutore}>                    
+                            </div>
+                            <div class="post-meta__data">
+                                <div class="post-meta__author">${nomeAutore}</div>
+                                <div class="post-meta__time">${posts[i].created}</div>
+                            </div>                    
+                        </div>
+                    </div>
+                    <div class="post__text">${posts[i].content}</div>
+                    <div class="post__image">
+                        <img src="${immagineAutore }" alt="">
+                    </div>
+                    <div class="post__footer">
+                        <div class="likes js-likes">
+                            <div class="likes__cta">
+                            <a class="like-button  js-like-button" data-postid="1">
+                                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                    <span class="like-button__label" id="testoTasto">Mi Piace</span>
+                                </a>
+                            </div>
+                            <div class="likes__counter">
+                                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                            </div>
+                        </div> 
+                    </div>            
+                </div>`
 
 }
 
-document.getElementById("bottoneLike").addEventListener("click",
-function (){
-     document.getElementById('testoTasto').style.color= "green";
+const bottoneMiPiace = document.getElementsByClassName('js-like-button');
 
+for ( let i = 0; i<bottoneMiPiace.length; i++){
+    bottoneMiPiace[i].addEventListener('Click',function(event){
+        event.preventDefault();
+        this.classList.add('like-button--liked');
+        const postid = this.getAttribute('data-postid');
+
+        const contatoreLike= document.getElementById('Like-counter-' + postid);
+        contatoreLike.innerText += 1;
+    
+});
 }
-
-
-);
-
 
 
 
